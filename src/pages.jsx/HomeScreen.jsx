7f0866d7a9b5bfe3e4098af4e6a2bc742e08956e
jitendra <jitendrasaini352jjnu@gmail.com>
2023-02-09 13:46:00 +0530

    implemented first row section differently

--- src/pages.jsx/HomeScreen.jsx
+++ src/pages.jsx/HomeScreen.jsx
@@ -12,9 +12,10 @@ const HomeScreen = () => {
         <Banner />
 
         {/* Row component for all fetching */}
-        <Row title="NetFlix Originals" fetchUrl = {requests.fetchNetflixOriginals} />
-        <Row title="Trending Now" fetchUrl = {requests.fetchTrending} />
-        <Row title="Top Rated" fetchUrl = {requests.fetchTopRated} />
+        {/* this one I want slightly bigger row */}
+        <Row title="NetFlix Originals" fetchUrl = {requests.fetchNetflixOriginals} isLargeRow />
+        <Row title="Trending Now" fetchUrl = {requests.fetchTrending}  />
+        <Row title="Top Rated" fetchUrl = {requests.fetchTopRated}  />
         <Row title="Action Movies" fetchUrl = {requests.fetchActionMovies} />
         <Row title="Comdey Novies" fetchUrl = {requests.fetchComedyMovies} />
         <Row title="Horror Movies" fetchUrl = {requests.fetchHorrorMovies} />
